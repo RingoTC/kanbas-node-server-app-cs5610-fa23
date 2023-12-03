52a52b1f3961ec5f695d948ef4dbf5af956047da
ringotc <ringotc@outlook.com>
2023-12-03 00:29:42 -0800

    a6

--- users/model.js
+++ users/model.js
@@ -1,7 +1,5 @@
 import schema from "./schema.js";
 import mongoose from "mongoose";
 
-// enforce all the  restriction in scheme for users.
-// this is where we can do SQL injection if needed.
-const model = mongoose.model("users", schema); // user is arbitray, its needs be unique to create instance.
+const model = mongoose.model("users", schema);
 export default model;
